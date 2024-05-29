07e1c40a7ef1d455bbc9d1348e64c9de1e946c1f
e-s-l <e-sullivan-lester@proton.me>
2024-05-29 22:39:07 +0200

    ok added a bfs algo to group similar brightnesses into shapes, not sure if actually works, definitely not optimised

--- img2ascii-v2/image2ascii.py
+++ img2ascii-v2/image2ascii.py
@@ -7,6 +7,8 @@
 # https://medium.com/@shubham0473/unleash-your-inner-artist-a-step-by-step-guide-to-converting-images-to-ascii-art-using-java-97860464f19a
 # and
 # https://github.com/isaksolheim/image-to-ascii/blob/master/image_to_ascii.py
+# and
+# https://www.youtube.com/watch?v=wUQbchYY80U
 ##################
 
 ##################
@@ -22,48 +24,45 @@ import os                   # operating system stuff (to get file extensions)
 from PIL import Image       # Python Image Library
 import numpy as np          # for the usual
 
-
-########
-debug = False   # print misc. outputs
-###
+#######################
 # control parameters:
+debug = True   # print misc. outputs
 printToConsole = False
 printToFile = False
-
-#
 scale = int(20)     # scale factor to shrink image size
-#
 ASCIICHARS = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"  # light to dark, left to right
 
 ############################################################
 
+##################
 
-def preProcessImage(imgFile):
-    #######################
+
+def preprocess_image(img_file):
     # some minor preprocessing
-    ###
-    fileName, fileExtension = os.path.splitext(imgFile)
+
+    img = None  # initialise
+    fileName, fileExtension = os.path.splitext(img_file)
 
     if debug:
         print(fileName+'\n', fileExtension)
-    
-    #######################
+    ##################
+
     # open image file
     try:
-        img = Image.open(imgFile)       
+        img = Image.open(img_file)
     except IOError as ioe:
         print("Error opening the image file: \n", ioe)
         print("Are u sure this is an image file? Better start again...")
         sys.exit(1)
     
-    #######################
+    ##################
     # Resize the image according to the output:
-    W,H = img.size
+    W, H = img.size
     if printToConsole:
         # get console size
         console_width = os.get_terminal_size().columns
-        aspect_ratio = W / H
-        new_height = int(console_width / aspect_ratio)
+        aspect_ratio = W/H
+        new_height = int(console_width/aspect_ratio)
         # resize
         img = img.resize((console_width, new_height)) 
     ###
@@ -76,10 +75,54 @@ def preProcessImage(imgFile):
 
     return img
 
-############################################################
+
+def get_brightness(r, g, b):
+    # Given a set of rgb values of a pixel, calculate brightness
+    # brightness = sum([r, g, b])/3             # simple average
+    brightness = 0.299*r + 0.587*g + 0.114*b    # luminance formula
+    return brightness
 
 
-def img2AsciiConvertor(img):
+def bfs_search(matrix, start, tolerance):
+
+    w, h = matrix.shape
+    queue = [start]
+    b_init = matrix[start]
+    group = []
+    visited = []
+
+    while queue:
+        x, y = queue.pop(0)
+        visited.append((x, y))
+        group.append((x, y))
+
+        for i in range(max(0, x-1), min(w, x+2)):
+            for j in range(max(0, y-1), min(h, y+2)):
+                if (i, j) not in visited:
+                    if abs(matrix[i, j] - b_init) <= tolerance:
+                        queue.append((i, j))
+
+    return group, visited
+
+
+def find_shapes(matrix, tolerance):
+    # BFS pathfinder to get subsets of similar brightness
+
+    w, h = matrix.shape
+    shapes = []
+    visited = []
+
+    for x in range(w):
+        for y in range(h):
+            if (x, y) not in visited:
+                shape, newly_visited = bfs_search(matrix, (x, y), tolerance)
+                shapes.append(shape)
+                visited.extend(newly_visited)
+
+    return shapes
+
+
+def img2ascii_convertor(img):
 
     # get img (new) dimensions (again)
     W, H = img.size
@@ -93,27 +136,42 @@ def img2AsciiConvertor(img):
     for X in range(0, W):
         for Y in range(0, H):
             pos = (X, Y)                    # get position
-            pixelRGB = img.getpixel(pos)    # Get RGB
+            pixelRGB = img.getpixel(pos)    # get RGB
             R, G, B = pixelRGB
             # Calculate Brightness
             brightness = sum([R, G, B])/3
             # note: could (should?!) use luminance formula
             BM[X, Y] = brightness
+
     #
     if debug:
         print(BM)
 
-    #######################
+    ######################
+
+    shapes = find_shapes(BM, tolerance=10)
+    print(len(shapes))
+
+    sys.exit(1)
+
+
+    #initialise
+    ascii_matrix = np.full((w, h), ' ', dtype=str)
+
     # convert brightness into ascii
-    # wouldn't really want 1 ascii-char per pixel, but anyway...
+    for shape in shapes:
+        b_av = np.mean([bm[x, y] for x, y in group])
+        ascii_index = int((len(ASCIICHARS) - 1)*(b_av/255.0))
+        ascii_value = ASCIICHARS[ascii_index]
+        for x, y in group:
+            ascii_matrix[x, y] = ascii_value
+
     #######################
+    # print or save             #add option to print-save shape at a time
     output = ""
-    for Y in range(0, H):
-        for X in range(0, W):
-            brightness = BM[X, Y]
-            asciiIndex = int((len(ASCIICHARS) - 1) * (brightness / 255.0))
-            asciiValue = ASCIICHARS[asciiIndex]
-            output += asciiValue
+    for Y in range(H):
+        for X in range(W):
+            output += ascii_matrix[X, Y]
         output += "\n"
     #
     if debug:
@@ -126,7 +184,7 @@ def img2AsciiConvertor(img):
         f = open("imageAsAscii.txt", "wt")
         f.write(output)
         f.close()
-        if (debug):
+        if debug:
             print('printed to file')
     
     if printToConsole:
@@ -160,11 +218,11 @@ def main():
             print("Please enter 1 or 2, or 'file' or 'console'...")
 
     # if cli usage is: ./image2ascii.py imagefile.png
-    image_file = preProcessImage(sys.argv[1])
+    image_file = preprocess_image(sys.argv[1])
 
     #######################
     # call the runner
-    img2AsciiConvertor(image_file)
+    img2ascii_convertor(image_file)
 
 ############################################################
 
