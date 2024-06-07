0ccadb815e877e24bf9bbb0540e5d6ef01c221df
e-s-l <e-sullivan-lester@proton.me>
2024-06-07 19:44:08 +0200

    hmmmmm

--- pixel_sort_v1.py
+++ pixel_sort_v1.py
@@ -10,8 +10,10 @@ import os               # For terminal actions.
 from PIL import Image   # Python Image Library.
 import sys              # To exit the program, etc.
 import numpy as np      # For matrix maths.
-from collections import deque   # double-ended queue -  to reduce pop O
+import random
+from collections import deque   # doubled ended queue -  to reduce pop O
 from collections import defaultdict #
+
 
 # FUNCTIONS #
 
@@ -26,52 +28,54 @@ def open_image(image_file):
         print("Error opening the image file: \n", ioe)
         sys.exit(1)
 
-    # convert image to RGB type
+    # convert image to RBG type
     img = img.convert('RGB')
 
     return img
 
+
 def luminance(pix_rgb):
     """Given a set of rgb values of a pixel, calculate brightness."""
+
     r, g, b = pix_rgb
-    return 0.299 * r + 0.587 * g + 0.114 * b  # luminance formula
+    return 0.299*r + 0.587*g + 0.114*b    # luminance formula
+
 
 def bfs_search(matrix, start, tol, visited):
-    """Breadth First Search algorithm."""
+    """Breadth first search algorithm."""
 
     w, h = matrix.shape
     queue = deque([start])
-    init_brightness = matrix[start]
+    init = matrix[start]
     group = []
-
-    directions = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (1, 1), (-1, 1), (1, -1)]
 
     while queue:
         x, y = queue.popleft()
 
-        if visited[x, y]:
-            continue
+        if not visited[x, y]:
 
-        visited[x, y] = True
-        group.append((x, y))
+            visited[x, y] = True
+            group.append((x, y))
 
-        for dx, dy in directions:
-            nx, ny = x + dx, y + dy
-            if 0 <= nx < w and 0 <= ny < h and not visited[nx, ny]:
-                if abs(matrix[nx, ny] - init_brightness) <= tol:
-                    queue.append((nx, ny))
+            for i in range(max(0, x-1), min(w, x+2)):
+                for j in range(max(0, y-1), min(h, y+2)):
+                    if not visited[i, j] and abs(matrix[i, j] - init) <= tol:
+                        queue.append((i, j))
 
     return group
 
+
 def find_shapes(matrix):
     """Use the bfs pathfinder to get subsets of similar brightness."""
-    print("find_shapes")
 
+    print("find_shapes")
+    # initialise shapes and bfs stuff
     w, h = matrix.shape
     shapes = []     # list
     visited = np.zeros((w, h), dtype=bool)
-    tol = 200
+    tol = 20
 
+    # find shapes...
     for x in range(w):
         for y in range(h):
             if not visited[x, y]:
@@ -80,9 +84,25 @@ def find_shapes(matrix):
 
     return shapes
 
-def sort_pixels_by_brightness(pixels):
-    """Sort an array of pixels by their brightness."""
-    return sorted(pixels, key=luminance)
+
+def quick_sort(pixels):
+    """Recursive quick sort algorithm for Nx1 array of pixs"""
+
+    if not pixels:
+        return pixels
+    elif len(pixels) <= 1:
+        return pixels
+    else:
+        pivot = pixels[0]
+        # recursion...
+        lower = quick_sort([pix for pix in pixels[1:] if (luminance(pix) < luminance(pivot))])
+        higher = quick_sort([pix for pix in pixels[1:] if (luminance(pix) >= luminance(pivot))])
+        return lower + [pivot] + higher
+
+###
+
+
+###
 
 def runner(command_line_args):
     """Main function to run all components."""
@@ -93,6 +113,7 @@ def runner(command_line_args):
 
     # open (and preprocess) image file
     image = open_image(image_file)
+
     file_name, file_extension = os.path.splitext(image_file)
 
     # get dimensions of original image
@@ -104,12 +125,13 @@ def runner(command_line_args):
     # initialise new image
     new_image = Image.new('RGB', (w, h))
 
-    # initialise brightness matrix
+    # initialise brightness martix
     bm = np.zeros((w, h))
 
     # fill out the brightness matrix
     for x in range(w):
         for y in range(h):
+
             p = (x, y)
             pix = img_pixs[p]
             bm[x, y] = luminance(pix)
@@ -117,27 +139,57 @@ def runner(command_line_args):
     # based on the brightness, find shapes/areas of similar brightness
     shapes = find_shapes(bm)
 
-    ### Processing shapes ###
+    ###
+
     for shape in shapes:
+
         rows = defaultdict(list)
+
         for x, y in shape:
             rows[y].append((x, y))
 
+        rows = dict(rows)
+
+
+        all_pixels = []
+        all_sorted = []
+
+
         for y, coords in rows.items():
-            all_pixels = [img_pixs[(x, y)] for x, y in coords]
-            sorted_pixels = sort_pixels_by_brightness(all_pixels)
+
+
+            all_pixels.append([])
+
+            for x, y in coords:
+
+                 all_pixels.append(img_pixs[(x, y)])
+
+            sorted_pixels = quick_sort(all_pixels)
 
             for i, (x, y) in enumerate(coords):
                 new_image.putpixel((x, y), sorted_pixels[i])
 
+           # all_sorted.append(quick_sort(all_pixels[y]))
+
+           # for i in range(len(coords)):
+           #     p = coords[i]
+           #     new_image.putpixel(p, all_sorted[y][i])
+
+    ###
+
     # save end product
-    new_image.save(f"{file_name}_deranged.png")
+    new_image.save("%s_deranged.png" % file_name)
     # close original image file
     image.close()
+    #
     print(":)")
 
+
 if __name__ == '__main__':
+
+    ###
     # PARSE INPUT ARGUMENTS #
+    # Initialise argument parser:
     parser = argparse.ArgumentParser(description='Takes an image file and deranges it a la pixel sorting.')
 
     # Add arguments:
@@ -147,10 +199,13 @@ if __name__ == '__main__':
     # Parse:
     args = parser.parse_args()
 
+    ###
     # START RUNNING MAIN PROGRAM #
+    runner(args)
+
     try:
         os.system("figlet THE DERANGER")
-        runner(args)
+        #runner(args)
     except KeyboardInterrupt:
         sys.exit(0)
     except Exception as e:
